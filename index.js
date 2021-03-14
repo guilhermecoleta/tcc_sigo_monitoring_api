de46596a22853c0b0d0443ba04db526de151916d
Guilherme Coleta <guilhermepizzaia7@gmail.com>
2021-03-14 11:04:36 -0300

    Adicionando cors

--- index.js
+++ index.js
@@ -2,18 +2,24 @@
 var restify = require('restify');
 var monitoringController = require('./monitoringController.js');
 
+res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
 var server = restify.createServer({
     name: 'SIGO monitoring is running'
 });
 
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
+server.use(
+  function crossOrigin(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    return next();
+  }
+);
 
-/*---- SERVER ROUTES ----*/
 server.get('/monitors', monitoringController.monitoring);
 
-
-/*---- LISTENING ----*/
 server.listen(process.env.PORT || 8080, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
